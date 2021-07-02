da3b95584feb03f068da3bc9f4eb92a6b67cbb7f
Olya Gavrilko <olyaganeva@gmail.com>
2021-07-02 18:20:54 +0300

    Fix bug

--- VKClient/App/AppDelegate.swift
+++ VKClient/App/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let vc = Authorization()
+        let vc = AuthorizationViewController()
         let navigationController = UINavigationController(rootViewController: vc)
         window = UIWindow()
         window?.rootViewController = navigationController
