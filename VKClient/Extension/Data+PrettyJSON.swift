a7a18cf553b9b0eea25c4d7f46f2f9a8a195a629
Olya Gavrilko <olyaganeva@gmail.com>
2022-03-17 17:20:13 +0300

    Reset

---
+++ VKClient/Extension/Data+PrettyJSON.swift
@@ -0,0 +1,19 @@
+//
+//  Data+PrettyJSON.swift
+//  VKClient
+//
+//  Created by Olya Ganeva on 01.07.2021.
+//
+
+import Foundation
+
+extension Data {
+    var prettyJSON: NSString? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else {
+            return nil
+        }
+        return prettyPrintedString
+    }
+}
---
+++ VKClient/Extension/UIColor+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extension.swift
+//  VKClient
+//
+//  Created by Olya Ganeva on 15.02.2022.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static let vkBlueColor = UIColor(red: 45/255, green: 129/255, blue: 224/255, alpha: 1)
+    static let vkGrayColor = UIColor(red: 154/255, green: 162/255, blue: 173/255, alpha: 1)
+    static let vkLightGrayColor = UIColor(red: 240/255, green: 242/255, blue: 245/255, alpha: 1)
+}
